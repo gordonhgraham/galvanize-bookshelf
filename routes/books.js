3a767b5dcb9535327ef515bff02035e8ba834117
Gordon H Graham <theboss@gordonhgraham.com>
2016-09-20 16:36:06 -0600

    added router.get for all book in books.js, first test passes

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
+const humps = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get(`/books`, (req, res, next) => {
+  knex('books')
+    .orderBy('title')
+    .then(books => {
+      res.json(humps.camelizeKeys(books));
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
