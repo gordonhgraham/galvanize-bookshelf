24f03642c5ce308e8687b15054f77d1303eb008e
Gordon H Graham <theboss@gordonhgraham.com>
2016-09-20 17:25:43 -0600

    added router.get for one book in books.js, second test passes

--- routes/books.js
+++ routes/books.js
@@ -6,6 +6,7 @@ const humps = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+// get all books
 router.get(`/books`, (req, res, next) => {
   knex('books')
     .orderBy('title')
@@ -15,6 +16,19 @@ router.get(`/books`, (req, res, next) => {
   .catch((err) => {
     next(err);
   });
+});
+
+// get one book
+router.get(`/books/:id`, (req, res, next) => {
+  const id = req.params.id;
+  knex(`books`)
+    .where('id', id)
+    .then(books => {
+      res.json(humps.camelizeKeys(books[0]));
+    })
+    .catch((err) => {
+      next(err);
+    })
 });
 
 module.exports = router;
