f0f322120445d61d49311176a825ceddb199098b
Gordon H Graham <theboss@gordonhgraham.com>
2016-09-22 18:24:06 -0600

    added route for get and post request, first test passes

--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,33 @@
 'use strict';
 
 const express = require('express');
+var bcrypt = require(`bcrypt`);
+var knex = require(`../knex`)
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get(`/session`, (req, res, next) => {
+  if (req.session.userId) {
+    res.send(true);
+  } else {
+    res.send(false)
+  }
+});
+
+router.post(`/session`, (req, res, next) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(req.body.password, 12);
+  knex(`users`).insert({
+    email: req.body.email,
+    hashed_password: hash
+  }, `*`)
+  .then(result => {
+    const userId = result[0];
+    delete userId.password;
+    req.session.userId = userId;
+    res.send(userId);
+  })
+})
 
 module.exports = router;
