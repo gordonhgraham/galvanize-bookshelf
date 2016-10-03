585fcf7691b88cbc5c452d113e7f2a5e4cd7163c
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-03 14:35:41 -0600

    add migration and seed for favorites, tests pass

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,42 @@
 'use strict';
 
 const express = require('express');
+const knex = require(`../knex`);
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get(`/favorites`, (req, res) => {
+  if (req.session) {
+    res.type(`json`);
+    res.status(200).send('test')
+  } else {
+    res.status(401).send(`Unauthorized`);
+  }
+});
+
+router.get(`/favorites/check?bookId=`, (req, res) => {
+  if (req.session.userInfo) {
+
+  } else {
+    res.status(401).send(`Unauthorized`);
+  }
+});
+
+router.post(`/favorites`, (req, res) => {
+  if (req.session.userInfo) {
+
+  } else {
+    res.status(401).send(`Unauthorized`);
+  }
+});
+
+router.delete(`/favorites`, (req, res) => {
+  if (req.session.userInfo) {
+
+  } else {
+    res.status(401).send(`Unauthorized`);
+  }
+});
 
 module.exports = router;
