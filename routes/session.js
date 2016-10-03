158e8fe18a78c084dbc0d2d17e80cd49a208b98c
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-02 21:22:48 -0600

    update part 4, wrote code for router.post for signup not authentication...oops

--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,44 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require(`../knex`);
+const bodyParser = require(`body-parser`);
+var bcrypt = require(`bcrypt`);
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+// get session info
+router.get(`/session`, (req, res, next) => {
+  res.type(`json`);
+  if (req.session.userInfo) {
+    res.send(req.session.userInfo);
+  } else {
+    res.status(200).end('false');
+  }
+});
+
+//post session info
+router.post(`/session`, (req, res, next) => {
+  res.type(`json`);
+  const user = req.body;
+  const password = bcrypt.hashSync(req.body.password, 12);
+  knex(`users`)
+    .insert({
+      first_name: user.firstName,
+      last_name: user.lastName,
+      email: user.email,
+      hashed_password: password
+    }, `*`)
+    .then(data => {
+      req.session.userInfo = data[0];
+      delete data.hashed_password;
+      res.send(data[0]);
+    });
+});
+
+// delete session info
+// router.delete(`/session`, (req, res, next) => {
+//
+// });
 
 module.exports = router;
