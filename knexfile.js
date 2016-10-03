1bcef400d9cd6e7848d42551dc41fe6987d3aca1
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-03 16:58:49 -0600

    prepare heroku

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,16 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/bookshelf_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
+
+  "scripts": {
+    "knex": "knex",
+    "heroku-postbuild": "knex migrate:latest",
+    "nodemon": "nodemon server.js"
   }
 };
