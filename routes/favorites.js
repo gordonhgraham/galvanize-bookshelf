e1cc065b0951393a6b1f088f8200d1060b2d5524
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-04 16:19:00 -0600

    add route for .get favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,36 @@
 'use strict';
 
 const express = require('express');
+const knex = require(`../knex`);
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get(`/favorites`, (req, res) => {
+  const userInfo = req.session.userInfo;
+  const userId = userInfo.id;
+  knex(`favorites`)
+    .where(`user_id`, userId)
+    .join(`books`, `favorites.book_id`, `books.id`)
+    .returning(`books.id`)
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      return err;
+    })
+});
+
+// router.get(`/favorites` (req, res) => {
+//
+// });
+//
+// router.post(`/favorites` (req, res) => {
+//
+// });
+//
+// router.delete(`/favorites` (req, res) => {
+//
+// });
 
 module.exports = router;
