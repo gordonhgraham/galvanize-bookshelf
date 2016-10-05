01e305787642311f5fae053fe591a02f1a6e40ba
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-05 15:33:18 -0600

    add route to list book and get book by id

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const express = require(`express`)
+const knex = require(`../knex`)
+const { camelizeKeys, decamelizeKeys, } = require(`humps`)
+
 
 // eslint-disable-next-line new-cap
 const router = express.Router()
 
-// YOUR CODE HERE
+router.get(`/books`, (req, res) => {
+  knex(`books`)
+    .orderBy(`title`)
+    .then(data => {
+      const books = camelizeKeys(data)
 
+      res.send(books)
+    })
+    .catch(err => { return err })
+})
+
+router.get(`/books/:id`, (req, res) => {
+  const bookId = req.params.id
+
+  knex(`books`)
+    .where(`id`, bookId)
+    .then(data => {
+      const book = camelizeKeys(data[0])
+
+      res.send(book)
+    })
+    .catch(err => { return err })
+})
 module.exports = router
