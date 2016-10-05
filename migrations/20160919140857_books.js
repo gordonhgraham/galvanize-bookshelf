f8a83cd00db31d1e69faf2f1ca532145b5e420ec
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-05 14:46:07 -0600

    run migration and seed, both tests pass, lint project

--- migrations/20160919140857_books.js
+++ migrations/20160919140857_books.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 exports.up = function(knex) {
-  return knex.schema.createTable(`books`, (table) => {
-    table.increments();
-    table.string(`title`, 255).notNullable().defaultTo(``);
-    table.string(`author`, 255).notNullable().defaultTo(``);
-    table.string(`genre`, 255).notNullable().defaultTo(``);
-    table.text(`description`).notNullable().defaultTo(``);
-    table.text(`cover_url`).notNullable().defaultTo(``);
-    table.timestamps(true, true);
-  });
-};
+  return knex.schema.createTable(`books`, table => {
+    table.increments()
+    table.string(`title`, 255).notNullable().defaultTo(``)
+    table.string(`author`, 255).notNullable().defaultTo(``)
+    table.string(`genre`, 255).notNullable().defaultTo(``)
+    table.text(`description`).notNullable().defaultTo(``)
+    table.text(`cover_url`).notNullable().defaultTo(``)
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function(knex) {
-  return knex.schema.dropTable(`books`);
-};
+  return knex.schema.dropTable(`books`)
+}
