1319c594fb9d42ccccdff4de405710919514751d
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-05 16:29:51 -0600

    add routes for post and patch, tests pass

--- routes/books.js
+++ routes/books.js
@@ -8,7 +8,7 @@ const { camelizeKeys, decamelizeKeys, } = require(`humps`)
 // eslint-disable-next-line new-cap
 const router = express.Router()
 
-router.get(`/books`, (req, res) => {
+router.get(`/books`, (req, res, next) => {
   knex(`books`)
     .orderBy(`title`)
     .then(data => {
@@ -16,10 +16,10 @@ router.get(`/books`, (req, res) => {
 
       res.send(books)
     })
-    .catch(err => { return err })
+    .catch(err => { next(err) })
 })
 
-router.get(`/books/:id`, (req, res) => {
+router.get(`/books/:id`, (req, res, next) => {
   const bookId = req.params.id
 
   knex(`books`)
@@ -29,6 +29,36 @@ router.get(`/books/:id`, (req, res) => {
 
       res.send(book)
     })
-    .catch(err => { return err })
+    .catch(err => { next(err) })
 })
+
+router.post(`/books`, (req, res, next) => {
+  const submittedBook = decamelizeKeys(req.body)
+
+  knex(`books`)
+    .insert({
+      title: submittedBook.title,
+      author: submittedBook.author,
+      genre: submittedBook.genre,
+      description: submittedBook.description,
+      cover_url: submittedBook.cover_url,
+    }, `*`)
+    .then(data => {
+      const newBook = camelizeKeys(data[0])
+
+      res.send(newBook)
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+
+router.put(`books/:id`, (req, res, next) => {
+  const bookId = req.params.id
+  const submittedUpdate = decamelizeKeys(req.body)
+
+  knex
+
+})
+
 module.exports = router
