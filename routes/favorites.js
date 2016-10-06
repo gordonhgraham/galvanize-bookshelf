fb09cfb174af96b530a14257392c9c0ed2e81778
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-06 10:14:49 -0600

    add route for get favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,42 @@
 'use strict'
 
 const express = require(`express`)
+const knex = require(`../knex`)
+const cookieSession = require(`cookie-session`)
+const { camelizeKeys, decamelizeKeys, } = require(`humps`)
 
 // eslint-disable-next-line new-cap
 const router = express.Router()
 
-// YOUR CODE HERE
+router.get(`/favorites`, (req, res, next) => {
+  if (req.session.user) {
+    const userId = req.session.user.id
+
+    knex(`favorites`)
+      .innerJoin(`books`, `books.id`, `favorites.book_id`)
+      .where(`favorites.user_id`, userId)
+      .orderBy(`title`, `ASC`)
+      .then(data => {
+        const favorites = camelizeKeys(data)
+
+        res.send(favorites)
+      })
+      .catch(err => { next(err) })
+  } else {
+    next(err => { res.status(401).send(`Unauthorized`, err) })
+  }
+})
+
+// router.get(`/favorites/check?bookId=2`, (req, res, next) => {
+//  req.query.bookId
+// })
+//
+// router.post(`/favorites`, (req, res, next) => {
+//
+// })
+//
+// router.delete(`/favorites`, (req, res, next) => {
+//
+// })
 
 module.exports = router
