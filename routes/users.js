ac131cae9f3d7114052e25d5a65d17c501ffd443
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-05 19:04:35 -0600

    add routes for post new user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,54 @@
 'use strict'
 
 const express = require(`express`)
+const knex = require(`../knex`)
+const bcrypt = require(`bcrypt-as-promised`)
+const { camelizeKeys, decamelizeKeys } = require(`humps`)
 
 // eslint-disable-next-line new-cap
 const router = express.Router()
 
-// YOUR CODE HERE
+router.post(`/users`, (req, res, next) => {
+  const newUser = decamelizeKeys(req.body)
+  bcrypt.hash(req.body.password, 12)
+
+  knex(`users`)
+    .insert({
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      email: newUser.email,
+      hashed_password: hashed_password,
+    }, `*`)
+    .then(user => {
+      const addedUser = camelizeKeys(user[0])
+
+      res.send(addedUser)
+    })
+    .catch(err =>{
+      next(err)
+    })
+})
 
 module.exports = router
+
+
+
+// if (!newUser.email) {
+//   res.status(400).send(`Email must not be blank`)
+// }
+//
+// if (newUser.password < 8) {
+//   res.status(400).send(`Password must be at least 8 characters long`)
+// }
+// knex(`users`)
+//   .where(`email`, newUser.email)
+//   .first()
+//   .then(user => {
+//     console.log(`this is the existing user`, user)
+//     if (user) {
+//       res.status(400).send(`Email already exists`)
+//     }
+//   })
+//   .then(() => {
+//
+//   })
