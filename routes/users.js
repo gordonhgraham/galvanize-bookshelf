9f051fa7cdc200cebd6fe439e0e5cae3a7dad407
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-06 22:24:14 -0600

    added session after user signup

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const express = require(`express`)
 const knex = require(`../knex`)
 const bcrypt = require(`bcrypt-as-promised`)
 const { camelizeKeys, decamelizeKeys, } = require(`humps`)
+const cookieSession = require(`cookie-session`)
 
 /* eslint-disable new-cap, camelcase */
 const router = express.Router()
@@ -22,6 +23,7 @@ router.post(`/users`, (req, res, next) => {
         }, `*`)
         .then(user => {
           const addedUser = camelizeKeys(user[0])
+          req.session.user = addedUser
 
           delete addedUser.hashedPassword
           res.send(addedUser)
